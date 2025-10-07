8270dd478c9c896cbb0e1e6414ffc70fd76137b6
sajwan05 <surisajwan1998@gmail.com>
2025-10-07 10:01:35 +0000

    Add save function to save previous entries

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const save = document.querySelector(".save");
 const increment = document.querySelector(".increment");
 const result = document.querySelector(".result");
+
+const log = document.querySelector("#log");
 let count = 0;
 increment.addEventListener("click", countThePass);
 function countThePass () {
     count ++;
     result.textContent = count;
 }
+
+save.addEventListener("click", () => {
+    log.textContent += count+" - ";
+    result.textContent = 0;
+    count = 0;
+});
 
